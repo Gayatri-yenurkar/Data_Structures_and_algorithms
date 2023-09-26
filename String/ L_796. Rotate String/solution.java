fa09139a77127a68180af063006df21466238047
gayatri-yenurkar <gayatri.yenurkar@outlook.com>
2023-09-26 22:23:25 +0530

    adding ans

--- String/ L_796. Rotate String/solution.java
+++ String/ L_796. Rotate String/solution.java
@@ -1,3 +1,6 @@
-public class solution {
-    
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        return (s+s).contains(goal);
+    }
 }
