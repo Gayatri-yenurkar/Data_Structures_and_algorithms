fc78332d821c347b6816b6f36edb6da937e38b89
gayatri-yenurkar <gayatri.yenurkar@outlook.com>
2023-09-26 23:47:39 +0530

    sol

---
+++ String/L_2828. Check if a String Is an Acronym of Words/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAcronym(List<String> words, String s) {
+        if (s.length()==words.size()){
+            for (int i=0; i < s.length();i++){
+                if(s.charAt(i) != words.get(i).charAt(0)){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+        
+    }
+}
