4ee70bc9db72be0857a2bcc2be82db22f8d27c7d
gayatri-yenurkar <gayatri.yenurkar@outlook.com>
2023-09-26 22:24:19 +0530

    adding readme

--- String/L_387. First Unique Character in a String/solution.java
+++ String/L_387. First Unique Character in a String/solution.java
@@ -1,3 +1,16 @@
-public class solution {
-    
+class Solution {
+    public int firstUniqChar(String s) {
+
+         Map<Character, Integer> charFreq = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            charFreq.put(c, charFreq.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (charFreq.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
