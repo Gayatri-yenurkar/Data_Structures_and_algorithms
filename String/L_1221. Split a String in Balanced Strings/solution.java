e3700c3b830ecbb85b7082def7ae97d88a6e2266
gayatri-yenurkar <gayatri.yenurkar@outlook.com>
2023-09-28 20:06:52 +0530

    runtime 11ms

--- String/L_1221. Split a String in Balanced Strings/solution.java
+++ String/L_1221. Split a String in Balanced Strings/solution.java
@@ -1,3 +1,17 @@
-public class solution {
-    
+class Solution {
+    public int balancedStringSplit(String s) {
+        int result=0, count=0;
+        for (char c : s.toCharArray()){
+            if (c=='R'){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count==0){
+                result++;
+            }
+        }
+        return result;
+    }
 }
