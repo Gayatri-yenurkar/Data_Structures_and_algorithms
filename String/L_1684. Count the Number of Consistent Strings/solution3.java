98785e18bda7edb36c88c52394fc3dfea5ecacae
gayatri-yenurkar <gayatri.yenurkar@outlook.com>
2023-09-28 20:08:10 +0530

    runtime 14ms

--- String/L_1684. Count the Number of Consistent Strings/solution3.java
+++ String/L_1684. Count the Number of Consistent Strings/solution3.java
@@ -1,3 +1,27 @@
-public class solution3 {
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        boolean[] charSet = new boolean[26]; 
+
+        for (char ch : allowed.toCharArray()) {
+            charSet[ch - 'a'] = true;
+        }
+
+        int consistentCount = 0;
+
+        for (String word : words) {
+            boolean isConsistent = true;
+            for (char ch : word.toCharArray()) {
+                if (!charSet[ch - 'a']) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+            if (isConsistent) {
+                consistentCount++;
+            }
+        }
+
+        return consistentCount;
+    }
     
 }
